ec4a0f92d9d6ef48ae4d3194dff1ec82e5435062
Justin Dachille <justindachille@gmail.com>
2019-03-23 18:03:48 -0700

    added parsing of local tweets

--- scripts/main.js
+++ scripts/main.js
@@ -31,7 +31,7 @@ function answer(button) {
   var timeDiff = currentTime - startTime;
   var scoreText = document.getElementById("score");
   console.log("timeDiff: " + timeDiff);
-  var currentScore = 5000 / ((0.03* timeDiff) + 5);
+  var currentScore = 5000 / ((0.03 * timeDiff) + 5);
   score += currentScore;
   console.log("score: " + currentScore);
   scoreText.innerHTML = "Score: " + score;
@@ -45,6 +45,13 @@ function resetButtons() {
 }
 
 function parseLocalTweets() {
-  var parsedTweets = Papa.parse("resources/trumptweets.csv", config);
-  console.log(parsedTweets);
+//  var file = document.getElementById('myDOMElementId').files[0];
+  var url = "./resources/trumptweets.csv";
+//  var file = new File("resources/trumptweets.csv");
+  var parsedTweets = Papa.parse(url, {
+    download: true,
+    complete: function (results) {
+      console.log("Finished:", results.data);
+    }
+  });
 }
