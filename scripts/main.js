8a53494e7184c806111011287f3c50942991dff3
Justin Dachille <justindachille@gmail.com>
2019-03-23 19:10:13 -0700

    added real tweet text

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 var score;
 var questionCount;
 var startTime;
+var actualTweetResults;
 
 document.addEventListener('DOMContentLoaded', function () {
   initialize();
@@ -24,6 +25,7 @@ function showSplash() {
 function showGame() {
   document.getElementById("splash").style.display = "none";
   document.getElementById("game").style.display = "block";
+  setUpButtons();
 }
 
 function answer(button) {
@@ -36,13 +38,31 @@ function answer(button) {
   score += currentScore;
   console.log("score: " + currentScore);
   scoreText.innerHTML = "Score: " + score;
-  resetButtons();
+  setUpButtons();
 }
 
-function resetButtons() {
+function resetButtons(answerText1, answerText2) {
+  var randomNum = Math.random();
   var button1 = document.getElementById("answer1");
   var button2 = document.getElementById("answer2");
+
+
+  if (randomNum > 0.5) {
+    button1.innerHTML = answerText1;
+    button2.innerHTML = answerText2;
+  } else {
+    button1.innerHTML = answerText2;
+    button2.innerHTML = answerText1;
+  }
+
   startTime = Date.now();
+}
+
+function setUpButtons() {
+  var randomTweetNum = getRandomArbitrary(1, actualTweetResults.data.length);
+  randomTweetNum = Math.floor(randomTweetNum);
+  console.log("text: " + actualTweetResults.data[randomTweetNum][4]);
+  resetButtons(actualTweetResults.data[randomTweetNum][4], "FIXME");
 }
 
 function parseLocalTweets() {
@@ -52,13 +72,28 @@ function parseLocalTweets() {
   var parsedTweets = Papa.parse(url, {
     download: true,
     complete: function (results) {
-      for (var i in results.data) {
-        console.log(results.data[i][4]);
-      }
       parseTweetData(results);
-      console.log("Finished:", results.data);
     }
   });
 }
 
-function parseTweetData(results) {}
+function parseTweetData(results) {
+  actualTweetResults = results;
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive).
+ * The value is no lower than min (or the next integer greater than min
+ * if min isn't an integer) and no greater than max (or the next integer
+ * lower than max if max isn't an integer).
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
