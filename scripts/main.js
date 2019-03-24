a53f77c6bb95d07a6fcec8da20a9314658bc2167
Justin Dachille <justindachille@gmail.com>
2019-03-24 02:25:22 -0700

    added two more answer choices

--- scripts/main.js
+++ scripts/main.js
@@ -24,12 +24,14 @@ function enter() {
 
 function showSplash() {
   document.getElementById("splash").style.display = "block";
+  document.getElementById("score-div").style.display = "none";
   document.getElementById("game").style.display = "none";
   document.getElementById("results").style.display = "none";
 }
 
 function showGame() {
   document.getElementById("splash").style.display = "none";
+  document.getElementById("score-div").style.display = "block";
   document.getElementById("game").style.display = "block";
   document.getElementById("results").style.display = "none";
   setUpButtons();
@@ -37,6 +39,7 @@ function showGame() {
 
 function showResults() {
   document.getElementById("splash").style.display = "none";
+  document.getElementById("score-div").style.display = "block";
   document.getElementById("game").style.display = "none";
   document.getElementById("results").style.display = "block";
   document.getElementById("results-text").innerHTML = "Congratulations, game finished. You scored " + score.toFixed(0) + "!" + " \You got " + numCorrect + " correct out of " + totalQuestions + " total questions.";
@@ -65,10 +68,15 @@ function answer(button) {
     score += currentScore;
     numCorrect++;
   }
+
   var button1 = document.getElementById("answer1");
   var button2 = document.getElementById("answer2");
+  var button3 = document.getElementById("answer3");
+  var button4 = document.getElementById("answer4");
   button1.classList.remove("right");
   button2.classList.remove("right");
+  button3.classList.remove("right");
+  button4.classList.remove("right");
 
   if (questionCount === totalQuestions) {
     showResults();
@@ -85,20 +93,37 @@ function updateScoreText() {
   questionText.innerHTML = "Question " + questionCount + " Of " + totalQuestions;
 }
 
-function resetButtons(answerText1, answerText2) {
+function resetButtons(answerText1, answerText2, answerText3, answerText4) {
   var randomNum = Math.random();
   var button1 = document.getElementById("answer1");
   var button2 = document.getElementById("answer2");
+  var button3 = document.getElementById("answer3");
+  var button4 = document.getElementById("answer4");
 
-
-  if (randomNum > 0.5) {
+  if (randomNum < 0.25) {
     button1.innerHTML = answerText1;
     button2.innerHTML = answerText2;
-    button2.classList.add("right");
-  } else {
+    button3.innerHTML = answerText3;
+    button4.innerHTML = answerText4;
+    button1.classList.add("right");
+  } else if (randomNum >= 0.25 && randomNum <= 0.5) {
     button1.innerHTML = answerText2;
     button2.innerHTML = answerText1;
-    button1.classList.add("right");
+    button3.innerHTML = answerText3;
+    button4.innerHTML = answerText4;
+    button2.classList.add("right");
+  } else if (randomNum >= 0.5 && randomNum <= 0.75) {
+    button1.innerHTML = answerText3;
+    button2.innerHTML = answerText2;
+    button3.innerHTML = answerText1;
+    button4.innerHTML = answerText4;
+    button3.classList.add("right");
+  } else {
+    button1.innerHTML = answerText4;
+    button2.innerHTML = answerText2;
+    button3.innerHTML = answerText3;
+    button4.innerHTML = answerText1;
+    button4.classList.add("right");
   }
 
   startTime = Date.now();
@@ -106,6 +131,8 @@ function resetButtons(answerText1, answerText2) {
 
 function setUpButtons() {
   //Getting actual tweets
+  var randomTweetNumbers = getUniqueNumbersWithinRange(actualTweetResults.data.length, 3);
+  
   var randomTweetNum = getRandomArbitrary(1, actualTweetResults.data.length);
   randomTweetNum = Math.floor(randomTweetNum);
 
@@ -113,13 +140,18 @@ function setUpButtons() {
   var randomFakeTweetNum = getRandomArbitrary(1, fakeTweetResults.data.length);
   randomFakeTweetNum = Math.floor(randomFakeTweetNum);
 
-  var realTweet = actualTweetResults.data[randomTweetNum][4];
   var fakeTweet = fakeTweetResults.data[randomFakeTweetNum][0];
   fakeTweet = removeWhiteSpaces(fakeTweet);
+  var realTweet = actualTweetResults.data[randomTweetNumbers[1]][4];
+  console.log(realTweet);
 
   //  console.log(fakeTweetResults.data[2][0]);
   //  console.log("text: " + actualTweetResults.data[randomTweetNum][4]);
-  resetButtons(realTweet, fakeTweet);
+  resetButtons(
+    fakeTweet, 
+    actualTweetResults.data[randomTweetNumbers[0]][4], 
+    actualTweetResults.data[randomTweetNumbers[1]][4], 
+    actualTweetResults.data[randomTweetNumbers[2]][4]);
 }
 
 function removeWhiteSpaces(text) {
@@ -154,6 +186,15 @@ function parseTweetData(results) {
 function parseFakeTweetData(results2) {
   fakeTweetResults = results2;
   fakeTweetResultsData = results2.data;
+}
+
+function getUniqueNumbersWithinRange(range, numNumbers) {
+  var arr = []
+  while (arr.length < numNumbers) {
+    var r = Math.floor(Math.random() * range) + 1;
+    if (arr.indexOf(r) === -1) arr.push(r);
+  }
+  return arr;
 }
 
 function getRandomArbitrary(min, max) {
